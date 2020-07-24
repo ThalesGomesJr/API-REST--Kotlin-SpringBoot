74bf13154c0c5fd97c24445e3084671454cd0f4f
Thales Gomes <thales3238junior@gmail.com>
2020-07-23 20:56:28 -0400

    Iniciando o desenvolvimento

---
+++ register-medical-staff/src/main/kotlin/com/project/registermedicalstaff/RegisterMedicalStaffApplication.kt
@@ -0,0 +1,11 @@
+package com.project.registermedicalstaff
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RegisterMedicalStaffApplication
+
+fun main(args: Array<String>) {
+	runApplication<RegisterMedicalStaffApplication>(*args)
+}
---
+++ register-medical-staff/src/test/kotlin/com/project/registermedicalstaff/RegisterMedicalStaffApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.project.registermedicalstaff
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RegisterMedicalStaffApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
